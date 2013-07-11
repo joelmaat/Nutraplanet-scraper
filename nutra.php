d32504560a6be10457ab25727d5727fe78a9c47a
Joel Johnson <joelmaat@gmail.com>
2013-07-11 00:31:57 -0400

    Update nutra.php

--- nutra.php
+++ nutra.php
@@ -1,7 +1,6 @@
 <?php
 
 $page = @file_get_contents("http://www.nutraplanet.com/top_100");
-
 preg_match_all("%/product/(.*?)\.html%is", $page, $matches);
 
 $productUrlFragments = array_unique($matches[1]);
@@ -9,9 +8,8 @@ $productUrlFragments = array_unique($matches[1]);
 foreach($productUrlFragments as $productUrlFragment) {
 
     $page = @file_get_contents("http://www.nutraplanet.com/manufacturer/".$productUrlFragment."/reviews");
-
     preg_match_all("%<td>&nbsp;<i>(.*?)</i></td>%is", $page, $matches);
-
+    
     echo $productUrlFragment . "\t";
 
     foreach($matches[1] as $votes) {
