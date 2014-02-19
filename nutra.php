a0fe405cf97aa35518aeef78d154d1ff8c5d71b5
Joel Johnson <joelmaat@gmail.com>
2014-02-19 17:45:42 -0500

    Names to camelcase

--- nutra.php
+++ nutra.php
@@ -3,18 +3,18 @@
 $page = file_get_contents('http://www.nutraplanet.com/top_100');
 preg_match_all('%/product/(.*?)\.html%is', $page, $matches);
 
-$product_url_fragments = array_unique($matches[1]);
+$productUrlFragments = array_unique($matches[1]);
 
-foreach ($product_url_fragments as $product_url_fragment) 
+foreach ($productUrlFragments as $productUrlFragment) 
 {
 
     $page = file_get_contents('http://www.nutraplanet.com/manufacturer/'
-                                   .$product_url_fragment
+                                   .$productUrlFragment
                                    .'/reviews');
 
     preg_match_all('%<td>&nbsp;<i>(.*?)</i></td>%is', $page, $matches);
 
-    echo $product_url_fragment . '\t';
+    echo $productUrlFragment . '\t';
 
     foreach ($matches[1] as $votes) 
     {
